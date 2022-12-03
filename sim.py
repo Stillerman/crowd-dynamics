8da08b7af43bf71386f8d862f7b756fa6195460a
Jason Stillerman <jason.t.stillerman@gmail.com>
2022-12-03 16:20:21 -0500

    tada

--- sim.py
+++ sim.py
@@ -8,6 +8,7 @@ __docformat__ = "reStructuredText"
 # Python imports
 import random
 from typing import List
+import matplotlib.pyplot as plt
 
 # Library imports
 import pygame
@@ -18,45 +19,103 @@ import pymunk.pygame_util
 import numpy as np
 
 
-class BouncyBalls(object):
-    """
-    This class implements a simple scene in which there is a static platform (made up of a couple of lines)
-    that don't move. Balls appear occasionally and drop onto the platform. They bounce around.
-    """
+class CrowdSim(object):
 
-    def __init__(self) -> None:
-        # Space
-        self._space = pymunk.Space()
-        self._space.gravity = (0.0, 0.0)
+    def __init__(self, MAX_VEL=100) -> None:
 
-        # Physics
-        # Time step
-        self._dt = 1.0 / 60.0
-        # Number of physics steps per screen frame
-        self._physics_steps_per_frame = 1
+        # Params
+        self.MAX_VEL = MAX_VEL
+        self.WALL_LENGTH = 500
+        self.OFFSET = 10
+        self.EXIT = (self.OFFSET + self.WALL_LENGTH / 2,
+                     self.OFFSET + 0.95 * self.WALL_LENGTH)
 
-        # pygame
-        pygame.init()
-        self._screen = pygame.display.set_mode((600, 600))
-        self._clock = pygame.time.Clock()
+        self.history = []
 
-        self._draw_options = pymunk.pygame_util.DrawOptions(self._screen)
+        self._init_world()
 
-        # Static barrier walls (lines) that the balls bounce off of
-        self._add_static_scenery()
+        self.init_walls()
+        self.init_people()
 
-        # Balls that exist in the world
-        self._balls: List[pymunk.Circle] = []
 
-        # Execution control and time until the next ball spawns
-        self._running = True
-        self._ticks_to_next_ball = 10
+    def init_walls(self) -> None:
+        static_body = self._space.static_body
 
-    def run(self) -> None:
+        # Create segments around the edge of the screen.
+        l = self.WALL_LENGTH
+        offset = self.OFFSET
+
+        walls = [
+            pymunk.Segment(static_body, (offset, offset), (l, offset), 0.0),
+            pymunk.Segment(static_body, (l, offset), (l, l), 0.0),
+            pymunk.Segment(static_body, (l, l), (offset, l), 0.0),
+            pymunk.Segment(static_body, (offset, l), (offset, offset), 0.0),
+        ]
+
+        for wall in walls:
+            wall.elasticity = 0.95
+            wall.friction = 0.9
+        self._space.add(*walls)
+
+    def update_people(self) -> None:
+
+        # make people want to exit
+        for person,speed in self.people:
+            towards_exit = (np.asarray(self.EXIT) -
+                            np.asarray(person.body.position))
+            towards_exit = towards_exit / np.linalg.norm(towards_exit)
+            towards_exit = towards_exit * 500
+            person.body._set_force(tuple(towards_exit))
+
+            velocity = person.body._get_velocity()
+            if velocity.length > speed:
+                new_force = velocity / velocity.length * speed
+                person.body._set_velocity(new_force)
+
+        # Remove people within 15 pixels of the exit
+        people_to_remove = [(person,speed) for person,speed in self.people if np.linalg.norm(
+            np.asarray(person.body.position) - self.EXIT) < 15]
+        for person, speed in people_to_remove:
+            self._space.remove(person, person.body)
+            self.people.remove((person, speed))
+
+        # Add to history
+        self.history.append(len(self.people))
+
+        # stop running if no people left
+        if len(self.people) == 0:
+            self._running = False
+
+    def init_people(self) -> None:
         """
-        The main loop of the game.
+        Create a grid of people
         :return: None
         """
+        for i in range(20):
+            for j in range(20):
+                self.create_person(50 + i*20, 50 + j*20, random.randint(5, 10))
+
+    def create_person(self, x, y, r=5):
+        """
+        Create a person
+        :return: None
+        """
+        mass = 1
+        inertia = pymunk.moment_for_circle(mass, 0, r, (0, 0))
+        body = pymunk.Body(mass, inertia)
+        body.position = x, y
+        shape = pymunk.Circle(body, r, (0, 0))
+        shape.elasticity = 0.95
+        shape.friction = 0.0
+        self._space.add(body, shape)
+        speed = random.randint(50, 150)
+        self.people.append((shape, speed))
+
+
+    ### INTERNALS
+
+    def _run(self):
+
         # Main loop
         while self._running:
             # Progress time forward
@@ -64,7 +123,7 @@ class BouncyBalls(object):
                 self._space.step(self._dt)
 
             self._process_events()
-            self._update_balls()
+            self.update_people()
             self._clear_screen()
             self._draw_objects()
             pygame.display.flip()
@@ -72,79 +131,7 @@ class BouncyBalls(object):
             self._clock.tick(50)
             pygame.display.set_caption("fps: " + str(self._clock.get_fps()))
 
-    def _add_static_scenery(self) -> None:
-        """
-        Create the static bodies.
-        :return: None
-        """
-        static_body = self._space.static_body
-        static_lines = [
-            pymunk.Segment(static_body, (10, 10), (550,10), 0.0),
-            pymunk.Segment(static_body, (550, 10), (550, 550), 0.0),
-            pymunk.Segment(static_body, (550, 550), (10, 550), 0.0),
-            pymunk.Segment(static_body, (10, 550), (10, 10), 0.0),
-
-            pymunk.Segment(static_body, (10, 400), (250, 400), 0.0),
-            pymunk.Segment(static_body, (350, 400), (550, 400), 0.0),
-
-            
-        ]
-        for line in static_lines:
-            line.elasticity = 0.95
-            line.friction = 0.9
-        self._space.add(*static_lines)
-
-    def _process_events(self) -> None:
-        """
-        Handle game and events like keyboard input. Call once per frame only.
-        :return: None
-        """
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self._running = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                self._running = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
-                pygame.image.save(self._screen, "bouncing_balls.png")
-
-    def _update_balls(self) -> None:
-        """
-        Create/remove balls as necessary. Call once per frame only.
-        :return: None
-        """
-        self._ticks_to_next_ball -= 1
-        if self._ticks_to_next_ball <= 0:
-            self._create_ball()
-            self._ticks_to_next_ball = 10
-        # Remove balls that fall below 100 vertically
-        balls_to_remove = [ball for ball in self._balls if ball.body.position.y > 500]
-        for ball in balls_to_remove:
-            self._space.remove(ball, ball.body)
-            self._balls.remove(ball)
-
-    def _create_ball(self) -> None:
-        """
-        Create a ball.
-        :return:
-        """
-        mass = 10
-        radius = 25
-        inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
-        body = pymunk.Body(mass, inertia)
-        x,y  = random.randint(115, 350), random.randint(30, 300)
-
-        # ball_body.apply_impulse_at_local_point(Vec2d(*direction))
-
-        towards_exit = (np.asarray([x,y]) - np.asarray([400, 300])) * -10
-
-        body.apply_impulse_at_local_point(tuple(towards_exit))
-
-        body.position = x, y
-        shape = pymunk.Circle(body, radius, (0, 0))
-        shape.elasticity = 0.95
-        shape.friction = 0.9
-        self._space.add(body, shape)
-        self._balls.append(shape)
+        return self.history
 
     def _clear_screen(self) -> None:
         """
@@ -160,11 +147,55 @@ class BouncyBalls(object):
         """
         self._space.debug_draw(self._draw_options)
 
+    def _init_world(self):
+        # Space
+        self._space = pymunk.Space()
+        self._space.gravity = (0.0, 0.0)
+
+        # Physics
+        # Time step
+        self._dt = 1.0 / 60.0
+        # Number of physics steps per screen frame
+        self._physics_steps_per_frame = 1
+
+        # pygame
+        pygame.init()
+        self._screen = pygame.display.set_mode((600, 600))
+        self._clock = pygame.time.Clock()
+        self._draw_options = pymunk.pygame_util.DrawOptions(self._screen)
+        
+        # Execution control and time until the next ball spawns
+        self._running = True
+
+        # Balls that exist in the world
+        self.people: List[pymunk.Circle] = []
+
+
+    def _process_events(self) -> None:
+        """
+        Handle game and events like keyboard input. Call once per frame only.
+        :return: None
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self._running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                pygame.image.save(self._screen, "bouncing_balls.png")
+
+
+def sim():
+    sim = CrowdSim()
+    return sim._run()
 
 def main():
-    game = BouncyBalls()
-    game.run()
+    hists = [sim() for _ in range(5)]
 
+    for hist in hists:
+        plt.plot(hist)
+    
+    plt.show()
 
 if __name__ == "__main__":
     main()
